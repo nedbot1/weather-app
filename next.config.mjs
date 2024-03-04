52f7d844b96fb7615a41ada9815d06e0ba00ec47
Sangay <sdema6537@gmail.com>
2024-03-04 09:36:20 +0600

    add weather app file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import ('next').NextConfig} */
+// import { nextConfig, imagesConfig } from './your-config-file';
 
+// const nextConfig = {
+//   reactStrictMode: true,
+// // };
+
+// // const imagesConfig = {
+// //   images: {
+// //       domains: ['images.unsplash.com'],
+// //   },
+// // };
+
+// export { nextConfig as default 
+const nextConfig = {
+  images: {
+    domains: ["c1.wallpaperflare.com", "openweathermap.org"],
+  },
+};
 export default nextConfig;
